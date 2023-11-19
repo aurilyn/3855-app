c3c409fa429190ec907188e10f09b47bba1bc94b
aurilyn <caobenny122@gmail.com>
2023-11-19 10:21:08 -0800

    1

--- Receiver/receiver.py
+++ Receiver/receiver.py
@@ -9,17 +9,32 @@ import datetime
 import json
 import time
 from pykafka import KafkaClient
+import os
 
-headers = {"Content-Type": 'application/json; charset=utf-8'}
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
-hostname = "%s:%d" % (app_config['events']['hostname'], app_config['events']['port'])    
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
+headers = {"Content-Type": 'application/json; charset=utf-8'}
+hostname = "%s:%d" % (app_config['events']['hostname'], app_config['events']['port'])    
 
 max_retries = 100
 current_retries = 0
--- Storage/storage.py
+++ Storage/storage.py
@@ -15,9 +15,30 @@ from pykafka.common import OffsetType
 from threading import Thread
 from sqlalchemy import and_
 import time
+import os
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
 
 DB_ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@"
     f"{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/"
@@ -26,11 +47,6 @@ DB_ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{a
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-logger = logging.getLogger('basicLogger')
 
 def post_augment_stats(body):
 
