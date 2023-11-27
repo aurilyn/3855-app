1e452bc4daf3dc4c4e2514f3463d26ffb63474de
aurilyn <caobenny122@gmail.com>
2023-11-27 14:08:28 -0800

    yep

--- Health/health.py
+++ Health/health.py
@@ -6,6 +6,8 @@ import json
 import time
 import requests
 import logging
+from apscheduler.schedulers.background import BackgroundScheduler
+import yaml
 
 output = {
     "receiver": "",
@@ -15,31 +17,41 @@ output = {
     "last_update": "",
 }
 
+app_conf_file = "app_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
 logger = logging.getLogger('basicLogger')
 
 services = ['receiver', 'storage', 'processing', 'audit']
 
 def health_check():
-    while True:
-        for service in services:
-            try:
-                logger.info(f"Attempting to get health check from {service}")
-                response = requests.get(f"http://benny-3855.eastus2.cloudapp.azure.com/{service}/health", timeout=5)
-                if response.status_code == 200:
-                    output[service] = "running"
-                    logger.info(f"{service} is running")
-                else:
-                    logger.warning(f"{service} is down")
-                    output[service] = "down"
-            except:
+    for service in services:
+        try:
+            logger.info(f"Attempting to get health check from {service}")
+            response = requests.get(f"http://benny-3855.eastus2.cloudapp.azure.com/{service}/health", timeout=5)
+            if response.status_code == 200:
+                output[service] = "running"
+                logger.info(f"{service} is running")
+            else:
+                logger.warning(f"{service} is down")
                 output[service] = "down"
-        output["last_update"] = time.strftime("%Y-%m-%d %H:%M:%S")
-        save_to_json(output, "health_check_output.json")
-        time.sleep(20)
+        except:
+            output[service] = "down"
+    output["last_update"] = time.strftime("%Y-%m-%d %H:%M:%S")
+    save_to_json(output, "health_check_output.json")
 
 def save_to_json(data, filename):
     with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)    
+        json.dump(data, json_file, indent=4)
+
+def init_scheduler():
+    sched = BackgroundScheduler(daemon=True)
+    sched.add_job(health_check,
+                'interval',
+                seconds=app_config['scheduler']['period_sec'])
+    sched.start()
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 
