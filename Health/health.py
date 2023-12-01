5cbd310965cf1be7c7d17c00bc96a4f09fb69fd1
aurilyn <caobenny122@gmail.com>
2023-11-30 16:10:32 -0800

    yep

--- Health/health.py
+++ Health/health.py
@@ -18,9 +18,14 @@ output = {
 }
 
 app_conf_file = "app_conf.yml"
+log_conf_file = "log_conf.yml"
 
 with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
 
